77e705dbb7e592bc5da67d5d97b5ef8c512500e0
syncvi <syncvi@nonsense.com>
2022-04-24 09:46:08 -0400

    a typo

--- main.c
+++ main.c
@@ -56,7 +56,7 @@ int main(int argc, char* argv[]){
     syslog(LOG_NOTICE, "MAIN: Task has been started.");
 
     int choice;
-    //if flag requires additional argument, like -s [filesize], thus, another switch will use choiceOptionx
+    //if flag requires additional argument, like -s [filesize], thus, another switch will use choiceOption
     int choiceOption; 
     int isRecursive = 0;
 
