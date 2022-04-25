4eef5e08709c0531c41098c745c380c82e6c6b7d
danrok19 <danrok19@wp.pl>
2022-04-25 16:28:00 +0200

    add size

--- funnies.h
+++ funnies.h
@@ -1,8 +1,10 @@
 #ifndef FUNNIES_H
 #define FUNNIES_H
 #include "funnies.h"
+
 /*--------------FUNCTIONS GO THERE--------------*/
 void instruction_manual();
 void deleteAll(char* givenPath);
 time_h timeGetter(char *time);
+off_t sizeGetter(char *size);
 #endif
