c66f48c9d63b5b1ad42f8e3cf10b6e35415671ec
syncvi <syncvi@nonsense.com>
2022-04-25 10:29:47 -0400

    fun

--- funnies.h
+++ funnies.h
@@ -1,8 +1,22 @@
 #ifndef FUNNIES_H
 #define FUNNIES_H
 #include "funnies.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <syslog.h>
+#include <sys/sendfile.h>
 /*--------------FUNCTIONS GO THERE--------------*/
 void instruction_manual();
 void deleteAll(char* givenPath);
-time_h timeGetter(char *time);
+time_t timeGetter(char *time);
 #endif
