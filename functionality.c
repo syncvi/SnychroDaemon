1eb88f49277a4085a7066154c80277f4826ff2e5
Hituh <okazysssie@gmail.com>
2022-04-25 13:06:55 +0200

    push

--- functionality.c
+++ functionality.c
@@ -11,7 +11,23 @@
 void instruction_manual()
 {
     syslog(LOG_NOTICE, "Displaying manual page...");
-    fprintf(stdout, "This is where functionality will be explained...one day. (lmao) (rofl)");
+    char *filename = "manual.txt";
+    FILE *fp = fopen(filename, "r");
+
+    if (fp == NULL)
+    {
+        printf("Error: could not open file %s", filename);
+        return 1;
+    }
+
+    // read one character at a time and
+    // display it to the output
+    char ch;
+    while ((ch = fgetc(fp)) != EOF)
+        putchar(ch);
+
+    // close the file
+    fclose(fp);
 }
 void deleteAll(char* givenPath)
 {
